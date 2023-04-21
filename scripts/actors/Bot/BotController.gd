dee1de423f1403eeb385b0f409b6f1cbbb7994d4
ThePhard <47338095+PaulHarden@users.noreply.github.com>
2023-04-21 16:39:50 -0400

    Grabbed some components as onready variables and scripted movement print statements

--- scripts/actors/Bot/BotController.gd
+++ scripts/actors/Bot/BotController.gd
@@ -2,13 +2,27 @@ class_name BotController
 extends Node3D
 
 @export var moveSpeed:float
+var velocity = Vector3()
 @export var health:float
 @export var healthRate:float
 @export var detectDistance:float
 @export var projectile:Node3D #TODO: make this our own class
+@onready var animPlayer = get_node("AnimationPlayer")
+@onready var audioPlayer = get_node("AudioStreamPlayer3D")
 
-func _ready():
-	pass
-
-func _process(delta):
-	pass
+func _physics_process(delta):
+	_input(InputEvent)
+	
+func _input(InputEvent):
+	if Input.is_action_pressed("move_right"):
+		print("moving right")
+	elif Input.is_action_pressed("move_left"):
+		print("moving left")
+	elif Input.is_action_pressed("move_backward"):
+		print("moving backwards")
+	elif Input.is_action_pressed("move_forward"):
+		print("moving forward")
+	else:
+		velocity.x = 0
+		velocity.z = 0
+		print("not moving")
