fb9ab1deecbc1bd3437911deaecf89fa9e300f56
ThePhard <47338095+PaulHarden@users.noreply.github.com>
2023-04-21 16:15:25 -0400

    BotController script
    - Added a bunch of variables we might need for behavior/stats.
    - Added omnilight back to Bot eye.

---
+++ scripts/actors/Bot/BotController.gd
@@ -0,0 +1,14 @@
+class_name BotController
+extends Node3D
+
+@export var moveSpeed:float
+@export var health:float
+@export var healthRate:float
+@export var detectDistance:float
+@export var projectile:Node3D #TODO: make this our own class
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
