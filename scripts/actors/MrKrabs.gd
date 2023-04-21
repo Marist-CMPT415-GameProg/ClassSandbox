350fb506cfae49faf733f9746183b80aa953a539
Jonah Caro <jonah.caro1@marist.edu>
2023-04-21 16:40:32 -0400

    Started movement script

--- scripts/actors/MrKrabs.gd
+++ scripts/actors/MrKrabs.gd
@@ -23,4 +23,27 @@ func change_state(state):
 			speed = 1.5
 
 func _physics_process(delta):
+	var target = $NavigationAgent3D.get_next_location()
+	var pos = get_global_transform().origin
 	
+	var n = $RayCast3D.get_collision_normal()
+	if n.length_squared() < .001:
+		n = Vector3(0, 1, 0)
+		vel = (target - pos).slide(n).normalized() * speed
+		$Krabs.rotation.y = lerp_angle($Krabs.rotation.y, atan2(vel.x, vel.z), delta * 10)
+		
+		$NavigationAgent3D.set_velocity(vel)
+		move_and_slide()
+		
+func move_to_target(target_pos):
+	change_state("walking")
+	var closest_pos = NavigationServer3D.map_get_closest_point(get_world_3d().get_navigation_map(), target_pos)
+	$NavigationAgent3D.set_target_location(closest_pos)
+
+func get_random_pos(radius:float) -> Vector3:
+	var x1 = randi_range(-1,1)
+	var x2 = randi_range(-1,1)
+	
+	while x1 * x2 + x2 * x2 >= 1:
+		 x1 = randi_range(-1,1)
+		 x2 = randi_range(-1,1)
