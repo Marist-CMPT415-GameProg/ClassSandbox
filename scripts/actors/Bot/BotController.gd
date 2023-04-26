1eed733a7046b9bc62f10955d690469d81807a48
ThePhard <47338095+PaulHarden@users.noreply.github.com>
2023-04-26 15:29:11 -0400

    Scripted detecting the player node in the Outdoor scene

--- scripts/actors/Bot/BotController.gd
+++ scripts/actors/Bot/BotController.gd
@@ -1,28 +1,19 @@
 class_name BotController
-extends Node3D
+extends Node
 
 @export var moveSpeed:float
-var velocity = Vector3()
 @export var health:float
 @export var healthRate:float
 @export var detectDistance:float
 @export var projectile:Node3D #TODO: make this our own class
 @onready var animPlayer = get_node("AnimationPlayer")
 @onready var audioPlayer = get_node("AudioStreamPlayer3D")
+var direction:Vector3
+var player
 
-func _physics_process(delta):
-	_input(InputEvent)
-	
-func _input(InputEvent):
-	if Input.is_action_pressed("move_right"):
-		print("moving right")
-	elif Input.is_action_pressed("move_left"):
-		print("moving left")
-	elif Input.is_action_pressed("move_backward"):
-		print("moving backwards")
-	elif Input.is_action_pressed("move_forward"):
-		print("moving forward")
-	else:
-		velocity.x = 0
-		velocity.z = 0
-		print("not moving")
+func _ready():
+	player = get_node("../Player")
+
+func _physics_process(_delta):
+	animPlayer.play("walk")
+	#look_at(player, Vector3.UP)
