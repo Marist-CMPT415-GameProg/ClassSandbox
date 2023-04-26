5372e66960dc233ba31ba27a004bb0a1db6fe7c3
CJ Danyluk <Acemango935@gmail.com>
2023-04-26 14:34:28 -0400

    This Totally works, 100% no flaws whatsoever.

--- scripts/actors/Lich Scripts/Lich.gd
+++ scripts/actors/Lich Scripts/Lich.gd
@@ -4,16 +4,18 @@ extends CharacterBody3D
 @export var patDistance = 10
 @export var idleTime = 2
 @export var walkSpeed = 3
-@export var runSpeed = 7
+@export var runSpeed = 15
 
 var stateNum = 1;
 var stateReady:bool = true
 var idlePos
+var dir
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	idlePos = position
+	dir = -1
 	pass # Replace with function body.
 
 
@@ -22,7 +24,7 @@ func _process(delta):
 	if stateNum == 0:
 		idle(delta)
 	elif stateNum == 1:
-		patrol(delta)
+		patrol()
 	elif stateNum == 2:
 		chase(delta)
 	elif stateNum == 3:
@@ -38,14 +40,17 @@ func idle(delta):
 	if idleTime <= 0:
 		stateNum = 1
 		idleTime = 2
-	idleTime -= delta
+		dir *= -1
+	else:
+		idleTime -= delta
+		rotate(Vector3(0, 1, 0), 1.55*delta)
 	pass
 	
-func patrol(delta):
-	$"Lich Model/AnimationPlayer".play("Armature|Walk")
-	if idlePos.z + patDistance <= position.z:
+func patrol():
+	$"Lich Model/AnimationPlayer".play("Armature|Walk")		
+	if idlePos.z + patDistance <= position.z or idlePos.z - patDistance >= position.z:
 		stateNum = 0
-	velocity.z = -walkSpeed
+	velocity.z = walkSpeed * dir
 	move_and_slide()
 	pass
 
