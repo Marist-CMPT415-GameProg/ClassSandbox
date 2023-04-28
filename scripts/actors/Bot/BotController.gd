85b9b631ca3db5af5c042d52e2ef3bcdd3c42c48
ThePhard <47338095+PaulHarden@users.noreply.github.com>
2023-04-28 16:40:13 -0400

    Scripted creating dictionary of persistent data
    - Scripted creating dictionary of persistent data.
    - Created node group of "persistent" and added everything in scene.
    - Split health variable into current health and max health.
    - Created exported variable for ammo.

--- scripts/actors/Bot/BotController.gd
+++ scripts/actors/Bot/BotController.gd
@@ -2,9 +2,11 @@ class_name BotController
 extends CharacterBody3D
 
 @export var moveSpeed:float
-@export var health:float
-@export var healthRate:float
+@export var currentHealth:float
+@export var maxHealth:float
+@export var healingRate:float
 @export var detectDistance:float
+@export var ammo:float
 @export var projectile:Node3D #TODO: make this our own class
 @onready var animPlayer = get_node("AnimationPlayer")
 @onready var audioPlayer = get_node("AudioStreamPlayer3D")
@@ -16,4 +18,22 @@ func _ready():
 
 func _process(delta):
 	animPlayer.play("walk")
+	#TODO: move/rotate towards the player every frame
 	position = position.move_toward(player.position, moveSpeed * delta)
+
+func save_data():
+	var save_dict = {
+		"filename" : get_scene_file_path(),
+		"parent" : get_parent().get_path(),
+		"pos_x" : position.x,
+		"pos_y" : position.y,
+		"pos_z" : position.z,
+		"rot" : rotation,
+		"current_health" : currentHealth,
+		"max_health" : maxHealth,
+		"healing_rate" : healingRate,
+		"ammo" : ammo,
+		"move_speed" : moveSpeed,
+		"detect_distance" : detectDistance
+	}
+	return save_dict
