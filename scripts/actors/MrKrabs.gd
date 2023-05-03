c9923aa9897e894a6cb1572905ecbfdc90f24282
AJ-18 <arjun.suresh1@marist.edu>
2023-05-03 14:21:17 -0400

    Krab Save start

--- scripts/actors/MrKrabs.gd
+++ scripts/actors/MrKrabs.gd
@@ -22,6 +22,45 @@ func change_state(state):
 		#state_machine.travel("Take 001")
 	current_state = state
 
+func save():
+	var save_dict = {
+		"filename" : get_scene_file_path(),
+		"parent" : get_parent().get_path(),
+		"pos_x" : position.x, # Vector2 is not supported by JSON
+		"pos_y" : position.y,
+		"state" : current_state
+	}
+	return save_dict
+
+func save_game():
+	var save_game = FileAccess.open("user://savegame.save", FileAccess.WRITE)
+	var save_nodes = get_tree().get_nodes_in_group("Persist")
+	for node in save_nodes:
+		# Check the node is an instanced scene so it can be instanced again during load.
+		if node.scene_file_path.is_empty():
+			print("persistent node '%s' is not an instanced scene, skipped" % node.name)
+			continue
+
+		# Check the node has a save function.
+		if !node.has_method("save"):
+			print("persistent node '%s' is missing a save() function, skipped" % node.name)
+			continue
+
+		# Call the node's save function.
+		var node_data = node.call("save")
+
+		# JSON provides a static method to serialized JSON string.
+		var json_string = JSON.stringify(node_data)
+
+		# Store the save dictionary as a new line in the save file.
+		save_game.store_line(json_string)
+
+
+func load_data(data):
+	position = Vector3(data["pos.x"], data["pos.y"], data["pos.z"])
+
+
+
 func _physics_process(delta):
 	var target = $NavigationAgent3D.get_next_path_position()
 	var pos = get_global_transform().origin
